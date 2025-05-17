1815f3985b860025e97a4bceb57af9899b7d2a18
Ignacio García <ignacioiqq@gmail.com>
2025-05-17 18:14:11 -0400

    Endpoints for section 2 added.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Depends
 from sqlmodel import Session, SQLModel, create_engine, Field, text
-from typing import Annotated
+from typing import Annotated, Optional, List, Dict
 from datetime import datetime
 
 
 DATABASE_URL = (
-    "mssql+pyodbc://localhost/PracticeAPI"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
+    'mssql+pyodbc://localhost/PracticeAPI'
+    '?driver=ODBC+Driver+17+for+SQL+Server'
 )
 
 engine = create_engine(DATABASE_URL)
@@ -21,20 +21,20 @@ def get_session():
 session_dep = Annotated[Session, Depends(get_session)]
 
 
-class Job(SQLModel):
-    id: int = Field(primary_key=True, index=True)
+class Job(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     job : str
 
 
-class Hired(SQLModel):
-    id : int = Field(default=None, primary_key=True)
+class Employees(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name : str | None
     datetime : datetime | None
     department_id : int | None
     job_id : int | None
 
-class Department(SQLModel):
-    id : int = Field(default=None, primary_key=True)
+class Department(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     department : str
 
 
@@ -46,13 +46,16 @@ class DepartmentsBase(SQLModel, table=True):
 
 app = FastAPI()
 
+
+SQLModel.metadata.create_all(engine)
+
 @app.get('/')
 def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
 
 
 
-@app.get("/departmentsbase/", response_model=list[DepartmentsBase])
+@app.get('/departmentsbase/', response_model=list[DepartmentsBase])
 def get_departments(session: session_dep):
     sql = """
         SELECT *
@@ -64,3 +67,64 @@ def get_departments(session: session_dep):
     column_names = result.keys()
     data = [dict(zip(column_names, row)) for row in rows]
     return data
+
+
+
+@app.get('/hired/quarterly', response_model=List[Dict])
+def get_hired_table(session: session_dep):
+    sql = """
+        SELECT 
+            depsb.department,
+            jobb.job,
+            SUM(CASE WHEN DATEPART(QUARTER, heb.[datetime]) = 1 THEN 1 ELSE 0 END) AS Q1,
+            SUM(CASE WHEN DATEPART(QUARTER, heb.[datetime]) = 2 THEN 1 ELSE 0 END) AS Q2,
+            SUM(CASE WHEN DATEPART(QUARTER, heb.[datetime]) = 3 THEN 1 ELSE 0 END) AS Q3,
+            SUM(CASE WHEN DATEPART(QUARTER, heb.[datetime]) = 4 THEN 1 ELSE 0 END) AS Q4
+        FROM 
+            hired_employeesbase as heb
+        LEFT JOIN [dbo].[departmentsbase] as depsb
+            ON depsb.id = heb.department_id
+        LEFT JOIN [dbo].[jobsbase] as jobb
+            ON jobb.id = heb.job_id
+        WHERE 
+            YEAR(heb.[datetime]) = 2021
+            AND heb.department_id IS NOT NULL
+        GROUP BY 
+            depsb.department,
+            jobb.job
+        ORDER BY 
+            depsb.department,
+            jobb.job;
+    """
+    result = session.exec(text(sql))
+    rows = result.fetchall()
+    column_names = result.keys()
+    data = [dict(zip(column_names, row)) for row in rows]
+    return data
+
+
+@app.get('/hired/above-average-2021', response_model=List[Dict])
+def hired_above_average(session: session_dep):
+    sql = """
+        SELECT 
+            department_id as id,
+            depsb.department, 
+            COUNT(empb.id) AS hired
+        FROM [dbo].[hired_employeesbase] AS empb
+        LEFT JOIN [dbo].[departmentsbase] AS depsb
+            ON empb.department_id = depsb.id
+        WHERE YEAR(empb.datetime) = 2021
+        GROUP BY empb.department_id, depsb.department
+        HAVING COUNT(empb.id) > (
+            SELECT COUNT(id) / COUNT(DISTINCT department_id) 
+            FROM [dbo].[hired_employeesbase] 
+            WHERE YEAR(datetime) = 2021
+        )
+        ORDER BY COUNT(empb.id) DESC;
+    """
+
+    result = session.exec(text(sql))
+    rows = result.fetchall()
+    column_names = result.keys()
+
+    return [dict(zip(column_names, row)) for row in rows]
